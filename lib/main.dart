054d2d309bbd594ff35c7e4227b229c6a49492bd
Rafael <rhplemos@gmail.com>
2023-06-27 18:59:35 -0300

    feat: device preview

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,18 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:rick_and_morty_playground/home/http/http_bindings.dart';
 import 'package:rick_and_morty_playground/home/http/http_page.dart';
+import 'package:device_preview/device_preview.dart';
 
 import 'home/home_page.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(
+  DevicePreview(
+    enabled: !kReleaseMode,
+    builder: (context) => const MyApp(), // Wrap your app
+  ),
+);
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -16,6 +21,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
+      locale: DevicePreview.locale(context),
+      builder: DevicePreview.appBuilder,
       getPages: [
         GetPage(
             name: '/',
